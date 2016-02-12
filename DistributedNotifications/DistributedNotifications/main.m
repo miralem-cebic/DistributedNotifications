cf80756ebdff65f83f6feccf8434bae363c234a1
Miralem Cebic <mail@miralem-cebic.de>
2016-02-12 10:42:49 +0100

    init Xcode project - OS X Cocoa Template

---
+++ DistributedNotifications/DistributedNotifications/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  DistributedNotifications
+//
+//  Created by Miralem Cebic on 12/02/16.
+//  Copyright © 2016 Miralem Cebic. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
