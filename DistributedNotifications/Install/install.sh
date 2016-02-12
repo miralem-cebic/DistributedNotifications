750b2e089d8d3032d636cc8ef19837e30d0921e5
Miralem Cebic <mail@miralem-cebic.de>
2016-02-12 14:29:26 +0100

    install / uninstall scripts and daemon plist created

---
+++ DistributedNotifications/Install/install.sh
@@ -0,0 +1,91 @@
+#!/bin/sh
+#
+# install.sh
+#
+# Created by Miralem Cebic on 12/02/16.
+#
+# Copyright © 2016 Miralem Cebic. All rights reserved.
+#
+# The script installing the two agents
+#
+
+CURRENTDIR=`pwd`
+VERSION="0.0.1"
+
+echo "\n"
+echo "--------------"
+echo "->START INSTALLATION OF LaunchAgent-Daemon & LaunchDaemon-Daemon"
+
+
+if ! [ -d "/Library/Application Support/MCC-DistributedNotifications" ]
+    then
+        mkdir "/Library/Application Support/MCC-DistributedNotifications"
+        echo "--> Created Directory /Library/Application Support/MCC-DistributedNotifications"
+fi
+
+if ! [ -f "$CURRENTDIR/uninstall.sh" ]
+    then
+        echo "--> Can not find: uninstall.sh in: $CURRENTDIR"
+        exit(12)
+fi
+
+/bin/sh "$CURRENTDIR/uninstall.sh"
+
+
+echo "\n"
+echo "--------------"
+echo "->CHECK IF PLIST FILES ARE AVAILABLE"
+
+if ! [ -f "$CURRENTDIR/de.miralem-cebic.launchagent.plist" ]
+    then
+        echo "--> Can not find: de.miralem-cebic.launchagent.plist in: $CURRENTDIR"
+        exit(22)
+fi
+
+if ! [ -f "$CURRENTDIR/de.miralem-cebic.launchdaemon.plist" ]
+    then
+        echo "--> Can not find: de.miralem-cebic.launchdaemon.plist in: $CURRENTDIR"
+        exit(23)
+fi
+
+
+echo "\n"
+echo "--------------"
+echo "->CHECK IF APPS ARE AVAILABLE"
+
+if ! [ -f "$CURRENTDIR/LaunchAgent-Daemon.app" ]
+    then
+        echo "--> Can not find: LaunchAgent-Daemon.app in: $CURRENTDIR"
+        exit(32)
+fi
+
+if ! [ -d "$CURRENTDIR/LaunchDaemon-Daemon.app" ]
+    then
+        echo "--> Can not find: LaunchDaemon-Daemon.app in: $CURRENTDIR"
+        exit(33)
+fi
+
+echo "\n"
+echo "--------------"
+echo "->COPY PLIST FILES"
+
+cp "$CURRENTDIR/de.miralem-cebic.launchagent.plist" "/Library/LaunchAgents/de.miralem-cebic.launchagent.plist"
+cp "$CURRENTDIR/de.miralem-cebic.launchdaemon.plist" "/Library/LaunchDaemons/de.miralem-cebic.launchdaemon.plist"
+
+echo "\n"
+echo "--------------"
+echo "->COPY APPS"
+
+cp -R "$CURRENTDIR/LaunchAgent-Daemon.app" "/Library/Application Support/MCC-DistributedNotifications/LaunchAgent-Daemon.app"
+cp -R "$CURRENTDIR/LaunchDaemon-Daemon.app" "/Library/Application Support/MCC-DistributedNotifications/LaunchDaemon-Daemon.app"
+
+echo "\n"
+echo "--------------"
+echo "->STARTING AGENTS"
+
+sudo launchctl load -w "/Library/LaunchAgents/de.miralem-cebic.launchagent.plist"
+sudo launchctl load -w "/Library/LaunchDaemons/de.miralem-cebic.launchdaemon.plist"
+
+echo "\n"
+echo "--------------"
+echo "->END INSTALLATION OF LaunchAgent-Daemon & LaunchDaemon-Daemon"
---
+++ DistributedNotifications/Install/uninstall.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# uninstall.sh
+#
+# Created by Miralem Cebic on 12/02/16.
+#
+# Copyright © 2016 Miralem Cebic. All rights reserved.
+#
+# The script uninstalling the two agents
+#
+
+echo "\n"
+echo "--------------"
+echo "->START UNINSTALLATION OF LaunchAgent-Daemon & LaunchDaemon-Daemon"
+
+if launchctl list "/Library/LaunchAgents/de.miralem-cebic.launchagent.plist" &> /dev/null; then
+echo "--> Unloaded de.miralem-cebic.launchagent.plist"
+sudo launchctl unload "/Library/LaunchAgents/de.miralem-cebic.launchagent.plist"
+fi
+
+if launchctl list "/Library/LaunchDaemons/de.miralem-cebic.launchdaemon.plist" &> /dev/null; then
+echo "--> Unloaded de.miralem-cebic.launchdaemon.plist"
+sudo launchctl unload "/Library/LaunchDaemons/de.miralem-cebic.launchdaemon.plist"
+fi
+
+echo "\n"
+echo "--------------"
+echo "->REMOVING DIRECTORY at /Library/Application Support/MCC-DistributedNotifications"
+
+rm -Rf "/Library/Application Support/MCC-DistributedNotifications"
