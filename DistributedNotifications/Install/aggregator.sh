e732a864b514396bdba07f357e4eba34559b1c80
Miralem Cebic <mail@miralem-cebic.de>
2016-02-17 14:17:25 +0100

    removed Aggregator Scheme

---
+++ DistributedNotifications/Install/aggregator.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#set
+
+DESTINATIONDIR="$HOME/Desktop/Daemon-Builds/"
+
+if ! [ -d "$DESTINATIONDIR" ]
+then
+rm -Rf $DESTINATIONDIR
+fi
+
+mkdir $DESTINATIONDIR
+
+echo -e "\n\n---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||\n\n"
+
+cd $PWD
+cd Install
+#ls -al
+cp "de.miralem-cebic.launchagent.plist" "$DESTINATIONDIR/de.miralem-cebic.launchagent.plist"
+cp "de.miralem-cebic.launchdaemon.plist" "$DESTINATIONDIR/de.miralem-cebic.launchdaemon.plist"
+cp "install.sh" "$DESTINATIONDIR/install.sh"
+cp "uninstall.sh" "$DESTINATIONDIR/uninstall.sh"
+
+echo -e "\n\n---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||\n\n"
+
+cd $TARGET_BUILD_DIR
+#ls -al
+cp -R "LaunchAgent-Daemon.app" "$DESTINATIONDIR/LaunchAgent-Daemon.app"
+cp -R "LaunchDaemon-Daemon.app" "$DESTINATIONDIR/LaunchDaemon-Daemon.app"
