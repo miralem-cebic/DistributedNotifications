33b6f63982f11c16ddc44898fa90ecf08c1e53b3
Miralem Cebic <mail@miralem-cebic.de>
2016-02-17 11:17:09 +0100

    uninstall script with killall call

--- DistributedNotifications/Install/uninstall.sh
+++ DistributedNotifications/Install/uninstall.sh
@@ -13,18 +13,28 @@ echo "\n"
 echo "--------------"
 echo "->START UNINSTALLATION OF LaunchAgent-Daemon & LaunchDaemon-Daemon"
 
+
 if launchctl list "/Library/LaunchAgents/de.miralem-cebic.launchagent.plist" &> /dev/null; then
 echo "--> Unloaded de.miralem-cebic.launchagent.plist"
 sudo launchctl unload "/Library/LaunchAgents/de.miralem-cebic.launchagent.plist"
+sudo rm "/Library/LaunchAgents/de.miralem-cebic.launchagent.plist"
 fi
+
 
 if launchctl list "/Library/LaunchDaemons/de.miralem-cebic.launchdaemon.plist" &> /dev/null; then
 echo "--> Unloaded de.miralem-cebic.launchdaemon.plist"
 sudo launchctl unload "/Library/LaunchDaemons/de.miralem-cebic.launchdaemon.plist"
+sudo rm "/Library/LaunchDaemons/de.miralem-cebic.launchdaemon.plist"
 fi
+
+
+sudo killall LaunchAgent-Daemon
+sudo killall LaunchDaemon-Daemon
+
 
 echo "\n"
 echo "--------------"
 echo "->REMOVING DIRECTORY at /Library/Application Support/MCC-DistributedNotifications"
+
 
 sudo rm -Rf "/Library/Application Support/MCC-DistributedNotifications"
